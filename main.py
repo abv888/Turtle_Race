12adbb57bd45e79b6db94ce84af01fe6eaeddfcc
Bagrat Arutyunov <arutyunovbagrat@gmail.com>
2024-02-13 12:34:17 +0300

    game logic

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import random
+from turtle import Turtle, Screen
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+turtles_list = []
+
+is_race_on = False
+
+for turtle_index in range(0, 6):
+    t = Turtle(shape="turtle")
+    t.color(colors[turtle_index])
+    t.penup()
+    t.goto(x=-230, y=-25 * turtle_index)
+    turtles_list.append(t)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in turtles_list:
+        if turtle.xcor() > 230:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost. The {winning_color} turtle is the winner!")
+            is_race_on = False
+        dist = random.randint(0, 10)
+        turtle.forward(dist)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.exitonclick()
